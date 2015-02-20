bdf278ff52b206152a9bceea312bc3a3a29c47ee
Todd Waits <twaits@mac-tokoapple.local>
2015-02-20 13:46:02 -0500

    added authentication to the requests in the front end

--- front_end.py
+++ front_end.py
@@ -3,11 +3,12 @@
 
 from flask import Flask, render_template
 import requests
+from requests.auth import HTTPBasicAuth
 app = Flask(__name__)
 
 @app.route("/")
 def display_movies():
-    r = requests.get('http://localhost:5000/api/movies')
+    r = requests.get('http://localhost:5000/api/movies', auth=('twaits', 'Passphrase1'))
     movies = r.json()['movies']
     return render_template('base.html', movies=movies)
 
@@ -15,10 +16,10 @@ def display_movies():
 @app.route("/movies/<int:movie_id>")
 def display_movie(movie_id):
     url = "http://localhost:5000/api/movies/" + str(movie_id)
-    r = requests.get(url)
+    r = requests.get(url, auth=('twaits', 'Passphrase1'))
     movie = r.json()['movie']
     return render_template('movie.html', movie=movie)
 
 
 if __name__ == "__main__":
-    app.run(port=5001)
+    app.run(port=5001, debug=True)
